37742ba01c8fd25faf900030c1ba17b8312cee77
Louis Narmour <lnarmour@rams.colostate.edu>
2019-01-31 18:05:26 -0700

    prepping commander

--- parallelTests/compile.sh
+++ parallelTests/compile.sh
@@ -1,1 +1,1 @@
-for x in firstLoop secndLoop thirdLoop; do  cd $x && make CC=icc CFLAGS="-O3 -xhost -ipo -fma -qopenmp"; cd ..; done
+for x in firstLoop secndLoop thirdLoop; do  cd $x && make CC=icc CFLAGS="-O3 -xhost -ipo -fma -fopenmp"; cd ..; done
