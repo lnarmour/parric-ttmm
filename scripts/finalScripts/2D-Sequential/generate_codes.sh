bc37dc22c4554ee6a2946c5316ccd1094a341c13
Louis Narmour <lnarmour@rams.colostate.edu>
2019-05-11 18:19:18 -0600

    adding da good flags to 2D-sequential scripts

--- scripts/finalScripts/2D-Sequential/generate_codes.sh
+++ scripts/finalScripts/2D-Sequential/generate_codes.sh
@@ -7,7 +7,7 @@ do
     ./compile.sh TMMScript.cs &> /dev/null;
     printf "making...";
     cd out;
-    make CC=icc &> /dev/null;
+    make CC=icc CFLAGS="-O3 -xhost -ipo -fma" &> /dev/null;
     printf "done.\n";
     cd ../../../;
 done;
@@ -19,7 +19,7 @@ do
     ./compile.sh TMMScript.cs &> /dev/null;
     printf "making...";
     cd out;
-    make CC=icc &> /dev/null;
+    make CC=icc CFLAGS="-O3 -xhost -ipo -fma" &> /dev/null;
     printf "done.\n";
     cd ../../../;
 done;
