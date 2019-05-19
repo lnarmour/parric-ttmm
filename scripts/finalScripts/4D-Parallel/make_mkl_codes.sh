9027620f8a71ce5fd57d3eebe1c96c9f7e4d18b1
Louis Narmour <lnarmour@rams.colostate.edu>
2019-05-19 10:43:20 -0600

    4D-parallel make script

--- scripts/finalScripts/4D-Parallel/make_mkl_codes.sh
+++ scripts/finalScripts/4D-Parallel/make_mkl_codes.sh
@@ -2,23 +2,23 @@
 
 for x in ./mklInterior/*/;
 do
+    printf "$(pwd) --> start\n"
     cd "${x}";
     printf "Making for ${x} Parallel(I) ...";
     make "CFLAGS=-O3  -std=c99  -I/usr/include/malloc/  -xhost -ipo -fma -fopenmp -DPARALLEL_I=1 -I/s/parsons/l/sys/intel/mkl/include/ -L/s/parsons/l/sys/intel/compilers_and_libraries_2019.3.199/linux/mkl/lib/intel64  -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core  -L/s/parsons/l/sys/intel/lib/intel64 -liomp5 -lm"  &> /dev/null;
-    mv BlockTMM BlockTMM_parallel_I;
-    mv BlockTMM.check BlockTMM_parallel_I.check
+    mv BlockTTMM BlockTTMM_parallel_I;
+    mv BlockTTMM.check BlockTTMM_parallel_I.check
     make clean &> /dev/null;
     printf "done.\n";
 
     printf "Making for ${x} Parallel(J) ...";
     make "CFLAGS=-O3  -std=c99  -I/usr/include/malloc/  -xhost -ipo -fma -fopenmp -DPARALLEL_J=1 -I/s/parsons/l/sys/intel/mkl/include/ -L/s/parsons/l/sys/intel/compilers_and_libraries_2019.3.199/linux/mkl/lib/intel64  -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core  -L/s/parsons/l/sys/intel/lib/intel64 -liomp5 -lm"  &> /dev/null;
-    mv BlockTMM BlockTMM_parallel_J;
-    mv BlockTMM.check BlockTMM_parallel_J.check
+    mv BlockTTMM BlockTTMM_parallel_J;
+    mv BlockTTMM.check BlockTTMM_parallel_J.check
     make clean &> /dev/null;
     printf "done.\n";
 
-
-
+    printf "$(pwd) --> end\n"
     cd ../../;
 done;
 
